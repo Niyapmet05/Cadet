7a7f904ff20f494e1751939c5f6eb2f98faa7af1
Aphrodis <niyomurengeziaphrodis@gmail.com>
2019-10-24 13:10:30 +0200

    Ap

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import express from 'express';
+/*import express from 'express';
 import bodyParser from 'body-parser';
 import routes from './routes/index.js';
 const app = express();
@@ -8,6 +8,21 @@ app.use('/', routes);
 
 app.listen(4000,() => {
     console.log(`app is listening to port 4000`);
-})
+})*/
 
-module.exports = app;
+// const app = express();
+const http = require('http');
+const port = process.env.PORT || 3000
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hello World</h1>');
+});
+
+server.listen(port,() => {
+  console.log(`Server running at port `+port);
+});
+
+//module.exports = app;
+// export default app;
